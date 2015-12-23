9b5261f1f908872eec48839e8b80b1887150276d
noscreenname <a.mandyev@gmail.com>
2015-12-23 23:12:07 +0100

    Use hibernate SchemaExport to generate sql for db creation (doesn't work with hibernate >= 5) Use hibernate SchemaExport to generate sql for db creation

--- crush-it/persistence/src/main/java/a/m/a/tools/SqlGenerator.java
+++ crush-it/persistence/src/main/java/a/m/a/tools/SqlGenerator.java
@@ -1,32 +1,55 @@
 package a.m.a.tools;
 
+import a.m.a.entity.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public final class SqlGenerator {
 
-    public static void main(String[] args) {
-        Configuration config = new Configuration();
+    private final Properties properties = new Properties();
 
-        Properties properties = new Properties();
+    private Configuration conf;
 
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        properties.put("hibernate.connection.url", "jdbc:postgresql://localhost:2101/crushit-dev");
-        properties.put("hibernate.connection.username", "root");
-        properties.put("hibernate.connection.password", "root");
-        properties.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        properties.put("hibernate.show_sql", "true");
-        config.setProperties(properties);
-
-//        config.addAnnotatedClass(MyMappedPojo1.class);
-//        config.addAnnotatedClass(MyMappedPojo2.class);
-
-        SchemaExport schemaExport = new SchemaExport(config);
-        schemaExport.setDelimiter(";");
-
-        /**Just dump the schema SQLs to the console , but not execute them ***/
-        schemaExport.create(true, false);
+    public SqlGenerator() {
     }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            SqlGenerator generator = new SqlGenerator();
+            generator.loadProperties("./hibernate.properties");
+            generator.initConfig();
+            generator.generate();
+        } catch (IOException e) {
+            System.out.println("Oops failed loading properties");
+            throw e;
+        }
+    }
+
+    private void loadProperties(String filePath) throws IOException {
+        properties.load(this.getClass().getClassLoader().getResourceAsStream(filePath));
+    }
+
+    private void initConfig() {
+        conf = new Configuration();
+        conf.addAnnotatedClass(AttemptEntity.class);
+        conf.addAnnotatedClass(CraigEntity.class);
+        conf.addAnnotatedClass(GradeEntity.class);
+        conf.addAnnotatedClass(GradeSystemEntity.class);
+        conf.addAnnotatedClass(RouteEntity.class);
+        conf.addAnnotatedClass(ZoneEntity.class);
+
+    }
+
+    private void generate() {
+        // <!> this doesn't work with hibernate 5
+        SchemaExport schemaExport = new SchemaExport(conf);
+        schemaExport.setDelimiter(";");
+        /**Just dump the schema SQLs to the console , but not execute them ***/
+        schemaExport.setOutputFile("schema-export.sql");
+        schemaExport.create(true, true);
+    }
+
 }
